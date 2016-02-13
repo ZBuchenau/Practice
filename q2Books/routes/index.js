72ced7bfc8990db8e051a61eb0c58dba89b062db
Zach Buchenau <zbuchenau@yahoo.com>
2016-02-13 10:05:50 -0700

    fixed a couple bugs

--- q2Books/routes/index.js
+++ q2Books/routes/index.js
@@ -7,35 +7,36 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/books', function(req, res, next){
+router.get('/books', function(req, res, next) {
   knex.select().from("book_info")
     .then(function(data) {
       // console.log(info);
-      res.render('books', {id: data[0].id, bookTitle: data[0].book_title, bookUrl: data[0].book_cover_url, bookDescription: data[0].book_description, bookGenre: data[0].book_genre});
+      res.render('books', {
+        data: data,
+        id: data[0].id,
+        bookTitle: data[0].book_title,
+        bookUrl: data[0].book_cover_url,
+        bookDescription: data[0].book_description,
+        bookGenre: data[0].book_genre
+      });
     });
 });
 
-router.post('/books/edit', function(req, res, next){
-  knex('book_info').insert(req.body)
-  .then(function(res){
-    // console.log(res);
-  });
-  res.send(req.body);
+router.get('/books/add', function(req, res, next) {
+  res.render('booksOverview');
 });
 
-router.post('/books/delete', function(req, res, next){
-  res.send('Delete Book Page');
-
-  res.render('deleteBook');
+router.get('/books/delete', function(req, res, next) {
+  res.render('booksOverview');
 });
 
-router.get('/author', function(req, res, next){
+router.get('/author', function(req, res, next) {
   res.send('Author Page');
 
   res.render('author');
 });
 
-router.get('/author/edit', function(req, res, next){
+router.get('/author/edit', function(req, res, next) {
   res.send('Edit Author Page');
 
   res.render('editAuthor');
